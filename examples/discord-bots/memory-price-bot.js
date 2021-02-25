26ac9a71082cda5b66054a036f441f124eed1b67
hatskier <suvorovalex1997@gmail.com>
2021-02-25 01:45:08 +0100

    bugfix in memory price bot

--- examples/discord-bots/memory-price-bot.js
+++ examples/discord-bots/memory-price-bot.js
@@ -3,7 +3,7 @@ const Limestone = require("../../index.js");
 const { runSimpleDiscordBot } = require("./simple-discord-bot");
 
 async function getCurrentARPricePerGB() {
-  const response = await axios.get("https://arweave.net/price/1048576");
+  const response = await axios.get("https://arweave.net/price/1073741824");
   return response.data * 0.000000000001;
 }
 
@@ -12,7 +12,7 @@ runSimpleDiscordBot({
     const priceFeed = await Limestone.getPrice("AR");
     const arPricePerGB = await getCurrentARPricePerGB();
     const usdPricePerGB = arPricePerGB * priceFeed.price;
-    const usdPricePerGBFormatted = +usdPricePerGB.toFixed(6);
+    const usdPricePerGBFormatted = +usdPricePerGB.toFixed(2);
     return `$${usdPricePerGBFormatted} = 1 GB`;
   },
   botToken: process.env.BOT_TOKEN,
