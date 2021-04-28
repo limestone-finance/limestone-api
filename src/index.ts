4e58dbb0775167b4cfed2dbc9b6b1944eb7bdca2
hatskier <suvorovalex1997@gmail.com>
2021-04-28 02:07:51 +0200

    symbols object added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import LimestoneApi from "./limestone-api";
 import LimestoneQuery from "./limestone-query";
+import symbols from "./symbols";
 
 class Limestone extends LimestoneApi {
   query = LimestoneQuery;
+  symbols = symbols;
   LimestoneApi = LimestoneApi;
 
   constructor(opts?: any) {
